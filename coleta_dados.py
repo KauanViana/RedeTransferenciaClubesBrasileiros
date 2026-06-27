205b122e2f9db84fd9e44081140ced095a2c170b
Kauan Viana <117935536+KauanViana@users.noreply.github.com>
2026-06-27 03:24:16 -0300

    Finish coleta_dados.py

--- coleta_dados.py
+++ coleta_dados.py
@@ -14,16 +14,43 @@ driver.get("https://www.transfermarkt.com.br/campeonato-brasileiro-serie-a/trans
 # Aguardar até que os elementos criados via JavaScript estejam presentes na página
 wait = WebDriverWait(driver, 10)
 
-# Extrair os elementos desejados (Clube de Destino) usando XPath
-clube_destino = wait.until(
+# Extrair os elementos desejados (Blocos de Transferências) usando XPath
+blocos_transferencias = wait.until(
     EC.presence_of_all_elements_located(
-        (By.XPATH, "//h2[contains(@class,'content-box-headline')]/a[2]")
+        (By.XPATH, "//div[@class='box']")
     )
 )
 
-# Imprimir os elementos extraídos
-for elemento in clube_destino:
-    print(elemento.get_attribute("textContent"))
+with open("transferencias.txt", "w", encoding="utf-8") as arquivo:
+
+    # Iterar pelos blocos de transferências desejados
+    for i in range(3, len(blocos_transferencias)):
+        # Encontra todos os links de clubes correspondentes dentro do bloco atual
+        clubes = blocos_transferencias[i].find_elements(By.XPATH, ".//h2[contains(@class, 'content-box-headline')]/a[2]")
+        
+        # Iterar pelos elementos encontrados dentro desse bloco específico
+        for clube in clubes:
+            nome_clube = clube.get_attribute("textContent").strip()
+            print(f'Movimentações do {nome_clube}')
+            arquivo.write(f'Movimentações do {nome_clube}\n')
+
+            # Iterar pelos elementos de movimentação dentro do bloco atual
+            for movimentacao in blocos_transferencias[i].find_elements(By.XPATH, "//span[@class='hide-for-small']"):
+                nome_jogador = movimentacao.get_attribute("textContent").strip()
+
+                # Escrever as informações no arquivo de saída
+                if movimentacao.find_element(By.XPATH, ".//ancestor::div[@class='box']//h2[contains(@class, 'content-box-headline')]/a[2]").get_attribute("textContent").strip() == nome_clube:
+                    if movimentacao.find_element(By.XPATH, ".//ancestor::div[@class='responsive-table']//th[@class='spieler-transfer-cell']").get_attribute("textContent").strip() == "Entradas":
+                        print(f'Nome: {nome_jogador} - Clube Destino: {nome_clube} - Clube Origem: {movimentacao.find_element(By.XPATH, ".//ancestor::tr//td[@class='no-border-links verein-flagge-transfer-cell']").get_attribute("textContent").strip()}')
+                        arquivo.write(f'Nome: {nome_jogador} - Clube Destino: {nome_clube} - Clube Origem: {movimentacao.find_element(By.XPATH, ".//ancestor::tr//td[@class='no-border-links verein-flagge-transfer-cell']").get_attribute("textContent").strip()}\n')
+                    elif movimentacao.find_element(By.XPATH, ".//ancestor::div[@class='responsive-table']//th[@class='spieler-transfer-cell']").get_attribute("textContent").strip() == "Saídas":
+                        print(f'Nome: {nome_jogador} - Clube Destino: {movimentacao.find_element(By.XPATH, ".//ancestor::tr//td[@class='no-border-links verein-flagge-transfer-cell']").get_attribute("textContent").strip()} - Clube Origem: {nome_clube}')
+                        arquivo.write(f'Nome: {nome_jogador} - Clube Destino: {movimentacao.find_element(By.XPATH, ".//ancestor::tr//td[@class='no-border-links verein-flagge-transfer-cell']").get_attribute("textContent").strip()} - Clube Origem: {nome_clube}\n')
+
+            # Separador entre clubes
+            print("\n-----\n")  
+            arquivo.write("\n-----\n\n")  
+
 
 # Fechar o driver do Selenium
 driver.quit()
